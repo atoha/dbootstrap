bf3db0f62d9cdbaa7467184621d530a8cd975cf6
Martin Pengelly-Phillips <dev@thesociable.net>
2012-10-06 09:43:14 +0100

    [#1] Add extra span node to nodes that don't support pseudo states.
    Allow font-awesome to work with incompatible Dijit templates.

--- source/dbootstrap/main.js
+++ source/dbootstrap/main.js
@@ -10,10 +10,28 @@ define([
 function (require) {
     var app = {};
 
-    require(['./Gallery', 'dojo/domReady!'], function (Gallery) {
-        app = new Gallery();
-        app.placeAt(document.body);
-        app.startup();
-    });
+    require(['./Gallery', 'dojo/query', 'dojo/dom-construct',
+             'dojo/dom-attr', 'dojo/domReady!'],
+        function (Gallery, query, domConstruct, domAttr) {
+            app = new Gallery();
+            app.placeAt(document.body);
+            app.startup();
+
+            // Fix up img tags to work with font icons. Have to do this
+            // as img does not support pseudo classes required by icon font.
+            query('img.dijitIcon, ' +
+                  'img.dijitTabStripIcon, ' +
+                  'img.dijitCalendarIncrementControl, ' +
+                  'input.dijitArrowButtonInner'
+            ).forEach(function(node) {
+                var span = domConstruct.create('span', {
+                    'class': domAttr.get(node, 'class')
+                }, node, 'before');
+            });
+
+            // Call resize so new nodes taken into account.
+            app.resize();
+        }
+    );
 });
 
