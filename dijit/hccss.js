6e65af638cd6cd35276722b63a544dfa90e8fb39
Martin Pengelly-Phillips <dev@thesociable.net>
2012-11-05 22:53:14 +0000

    Update to latest master build.

--- dijit/hccss.js
+++ dijit/hccss.js
@@ -1,2 +1,2 @@
 //>>built
-define("dijit/hccss",["dojo/dom-class","dojo/hccss","dojo/domReady","dojo/_base/window"],function(b,a,c,d){c(function(){a("highcontrast")&&b.add(d.body(),"dijit_a11y")});return a});
+define("dijit/hccss",["dojo/dom-class","dojo/hccss","dojo/ready","dojo/_base/window"],function(b,a,c,d){c(90,function(){a("highcontrast")&&b.add(d.body(),"dijit_a11y")});return a});
